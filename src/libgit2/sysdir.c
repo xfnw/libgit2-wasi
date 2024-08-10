5591a5759e40604c4c4901df3f0f5fa550324d24
xfnw <xfnw@riseup.net>
2024-08-10 12:35:05 -0400

    yoink out all the stuff that does not support wasi

--- src/libgit2/sysdir.c
+++ src/libgit2/sysdir.c
@@ -17,7 +17,6 @@
 # include "win32/utf-conv.h"
 #else
 # include <unistd.h>
-# include <pwd.h>
 #endif
 
 #ifdef GIT_WIN32
@@ -286,38 +285,7 @@ static int git_sysdir_guess_system_dirs(git_str *out)
 #ifndef GIT_WIN32
 static int get_passwd_home(git_str *out, uid_t uid)
 {
-	struct passwd pwd, *pwdptr;
-	char *buf = NULL;
-	long buflen;
-	int error;
-
-	GIT_ASSERT_ARG(out);
-
-	if ((buflen = sysconf(_SC_GETPW_R_SIZE_MAX)) == -1)
-		buflen = 1024;
-
-	do {
-		buf = git__realloc(buf, buflen);
-		error = getpwuid_r(uid, &pwd, buf, buflen, &pwdptr);
-		buflen *= 2;
-	} while (error == ERANGE && buflen <= 8192);
-
-	if (error) {
-		git_error_set(GIT_ERROR_OS, "failed to get passwd entry");
-		goto out;
-	}
-
-	if (!pwdptr) {
-		git_error_set(GIT_ERROR_OS, "no passwd entry found for user");
-		goto out;
-	}
-
-	if ((error = git_str_puts(out, pwdptr->pw_dir)) < 0)
-		goto out;
-
-out:
-	git__free(buf);
-	return error;
+	return -1;
 }
 #endif
 
